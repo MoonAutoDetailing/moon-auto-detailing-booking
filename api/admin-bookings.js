6c55cfa9b3e6f1071d2c32869ec05bef52c99043
Darren Moon <darrenmoon@Darrens-Air.lan>
2026-03-02 14:19:16 -0500

    last fix?

--- api/admin-bookings.js
+++ api/admin-bookings.js
@@ -44,9 +44,9 @@ if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
     phone
   ),
   vehicles (
-    year,
-    make,
-    model,
+    vehicle_year,
+    vehicle_make,
+    vehicle_model,
     vehicle_size
   ),
   service_variants (
